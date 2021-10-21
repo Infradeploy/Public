578b11885b56e13f3ac0da4dc56a91756654ca85
Infradeploy <ton.siemons@infradeploy.nl>
2021-10-21 10:16:48 +0200

    1st

---
+++ get-clientswf.ps1
@@ -0,0 +1,8 @@
+$localswf="$($env:localappdata)\Ubisoft\The Settlers Online\client.swf"
+$remoteswf = "https://sirris.tsomaps.com/client.swf"
+if (test-path $localswf){
+    if (( Invoke-WebRequest -method "Head" $remoteswf | Select -ExpandProperty Headers)["Content-length"] -ne (get-item $localswf).length){
+        "Newer file present, downloading"
+        Invoke-WebRequest -Uri $remoteswf -OutFile $localswf
+        }
+}
