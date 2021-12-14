ad9971ac6ead807bd1bdd6ddbc2621b09c397e1d
Ton Siemons <ton.siemons@infradeploy.nl>
2021-12-14 20:24:03 +0100

    try 5

--- get-clientswf.ps1
+++ get-clientswf.ps1
@@ -1,7 +1,7 @@
 $localswf="$($env:localappdata)\Ubisoft\The Settlers Online\client.swf"
 $remoteswf = "https://sirris.tsomaps.com/client.swf"
 if (test-path $localswf){
-    if (( Invoke-WebRequest -method "Head" $remoteswf | Select-Object -ExpandProperty Headers)["Content-length"] -ne (get-item $localswf).length){
+    if (( Invoke-WebRequest -method "Head" $remoteswf -usebasicparsing| Select-Object -ExpandProperty Headers)["Content-length"] -ne (get-item $localswf).length){
         "Newer file present, downloading"
         Invoke-WebRequest -Uri $remoteswf -OutFile $localswf
     }
