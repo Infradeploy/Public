9f1fae412fcce24366b40e6dfb9ff4f691b03ded
Ton Siemons <ton.siemons@infradeploy.nl>
2022-03-28 14:54:43 +0200

    MacOS added

--- get-clientswf.ps1
+++ get-clientswf.ps1
@@ -1,8 +1,19 @@
-$localswf="$($env:localappdata)\Ubisoft\The Settlers Online\client.swf"
+If($ismacos){
+    $localswf = "/Applications/TSOclient.app/Contents/Resources/client.swf"
+}
+Else{
+    $localswf="$($env:localappdata)\Ubisoft\The Settlers Online\client.swf"
+}
 $remoteswf = "https://sirris.tsomaps.com/client.swf"
 if (test-path $localswf){
     if (( Invoke-WebRequest -method "Head" $remoteswf -usebasicparsing| Select-Object -ExpandProperty Headers)["Content-length"] -ne (get-item $localswf).length){
         "Newer file present, downloading"
         Invoke-WebRequest -Uri $remoteswf -OutFile $localswf
     }
+    Else{
+        "No new file yet"
+    }
+}
+Else{
+    "local install not found"
 }
